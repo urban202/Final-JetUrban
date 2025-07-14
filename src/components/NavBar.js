2889cd3a1c05975dd6fb454f525ecca2b9e0f27d
urban202 <102764789+urban202@users.noreply.github.com>
2025-07-14 00:31:54 -0400

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,29 +4,22 @@ import logo from "../assets/logo2.png";
 
 const NavBar = () => {
   return (
-    
-   
     <nav className="navbar">
-        
       <img id="logo" src={logo} alt='logo' />
-    
-      {/* <button className="btn-wallet">Connect Wallet</button> */} 
 
-    <ul className="nav-links">
-    
-      
-      <div className="menu">
-        <li><a href="#p-collection" className="nav-link">ABOUT</a></li>
-        <li><a href="#p-ut" className="nav-link">UTILITIES</a></li>
-        <li><a href="#p-all" className="nav-link">ALLOCATIONS</a></li>
-        <li><a href="#p-roadmap" className="nav-link">ROAD MAP</a></li>
-        <li className="discord"><a href="https://discord.gg/nnq359KR" target="_blank" rel="noreferrer"  >DISCORD</a></li>
-        
-   
+      {/* <button className="btn-wallet">Connect Wallet</button> */}
 
-      </div>
-    </ul>
-  </nav>
+      <ul className="nav-links">
+        <div className="menu">
+          <li><a href="#p-collection" className="nav-link">ABOUT</a></li>
+          <li><a href="#p-ut" className="nav-link">UTILITIES</a></li>
+          <li><a href="#p-all" className="nav-link">ALLOCATIONS</a></li>
+          <li><a href="#p-roadmap" className="nav-link">ROAD MAP</a></li>
+          <li><a href="https://urbanwarriors.club/crypto-faq-bot.html" className="nav-link">Crypto &amp; NFT FAQ</a></li>
+          <li className="discord"><a href="https://discord.gg/nnq359KR" target="_blank" rel="noreferrer">DISCORD</a></li>
+        </div>
+      </ul>
+    </nav>
   )
 }
 
